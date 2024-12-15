c1c2c91528f5ef7aa495de5834113455178ffda3
Andrei <andrei_nasevich@senla.eu>
2024-12-15 16:47:52 +0300

    Initial commit for Robot framework

---
+++ src/by/teachmeskills/robot/hands/SonyHand.java
@@ -0,0 +1,24 @@
+package by.teachmeskills.robot.hands;
+
+public class SonyHand implements IHand {
+
+    private int price;
+
+    public  SonyHand(int price) {
+        this.price = price;
+    }
+
+    public  SonyHand() {
+    }
+
+    @Override
+    public void upHand() {
+        System.out.println("Гордо поднимаю Руку Сони");
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+}
